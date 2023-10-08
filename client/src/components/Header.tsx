cf19390581791b1d273d21cf63fd27053f3c1d3e
johnnyb <jbejar2001@yahoo.com>
2023-10-08 18:27:30 -0400

    updated title in header and index.html

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="flex justify-center items-center h-32 border-b border-b-slate-900 bg-slate-900">
       <span className="font-bold text-5xl mr-auto ml-4">
-        <Link to="/">Movies</Link>
+        <Link to="/">Film Docket</Link>
       </span>
       <ul className="flex gap-20 mr-4">
         <li className="text-3xl border-b border-slate-900 hover:border-white transition-all">
