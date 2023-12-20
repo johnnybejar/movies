b85f1a0b6e9bd00a775e3ccc623efbab8e14643d
johnnyb <jbejar2001@yahoo.com>
2023-12-19 23:28:56 -0500

    render search results with styling

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,20 +1,10 @@
 import React from "react";
+import { Movie } from "../types/movie";
 
-type MovieDetails = {
-  genre_ids: Array<number>;
-  id: string;
-  original_title: string;
-  overview: string;
-  poster_path: string;
-  release_date: string;
-  title: string;
-  vote_average: number;
-};
-
-function MovieCard(Movie: MovieDetails) {
+function MovieCard(props: Movie) {
   return (
     <>
-      <div>MovieCard</div>
+      <div>{props.title}</div>
     </>
   );
 }
--- client/src/pages/ListCreator.tsx
+++ client/src/pages/ListCreator.tsx
@@ -1,23 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import searchMovie from "../features/lists/searchService";
 import { Search } from "../types/search";
 import { Movie } from "../types/movie";
+import MovieCard from "../components/MovieCard";
 
 function ListCreator() {
   const [list, setList] = useState([]);
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Movie[]>([]);
   const [search, setSearch] = useState<string>("");
+  const [isLoadingResults, setLoadingResults] = useState(false);
+
+  const imgBaseUrl = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/";
 
   async function getSearchResults() {
-    const res: Search = await searchMovie(search);
-    console.log(res.results);
-    setResults(res.results);
+    // Clears results in case it is already set
+    setResults([]);
+    setLoadingResults(true);
+
+    const res: Promise<unknown> = searchMovie(search);
+
+    res.then((r: Search) => {
+      const movies: Movie[] = r.results as Movie[];
+      setResults((oldArr) => [...oldArr, ...movies]);
+    });
+
+    setLoadingResults(false);
   }
 
   const onChange = (e: React.ChangeEvent) => {
     const element = e.currentTarget as HTMLInputElement;
     setSearch(() => element.value);
   };
+
+  if (isLoadingResults) {
+    return <>Loading...</>;
+  }
 
   return (
     <>
@@ -43,7 +60,30 @@ function ListCreator() {
           Search
         </button>
       </div>
-      <div>{results ? <>stuff</> : <>nothing</>}</div>
+      <div className="flex gap-2 flex-wrap w-3/4 justify-center">
+        {results.length === 0 ? (
+          <></>
+        ) : (
+          <>
+            {results.map((movie) => {
+              return (
+                <div key={movie.id} className="flex flex-col items-center">
+                  <img
+                    src={imgBaseUrl + movie.poster_path}
+                    alt={movie.title}
+                    className="flex h-64 w-44 border-2 border-white hover:opacity-50 transition-all"
+                  />
+                  <span className="w-44 text-center">
+                    {movie.title === movie.original_title
+                      ? movie.title
+                      : movie.title}
+                  </span>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
     </>
   );
 }
