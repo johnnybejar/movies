0a9ae33e3bcf2cd8fd9e9de4a635d2ff680c8fb8
johnnyb <jbejar2001@yahoo.com>
2023-12-22 20:59:46 -0500

    refactored genres to a map + added genres to MovieCards + vote count rendered next to rating

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Movie } from "../types/movie";
 import { IoMdStar } from "react-icons/io";
+import { genres } from "../data/genres";
+import { Movie } from "../types/movie";
 
 type MovieCardProps = {
   movie: Movie;
@@ -17,15 +18,26 @@ function MovieCard(props: MovieCardProps) {
         className="h-46 w-32"
       ></img>
       <div className="flex flex-col items-start gap-1 pl-1 max-h-52">
-        <span className="text-center underline">{props.movie.title}</span>
+        <div>
+          <span className="text-center underline">{props.movie.title}</span>
+        </div>
+        <span>
+          {props.movie.genre_ids.map(
+            (id, index) =>
+              genres.get(id) +
+              (index === props.movie.genre_ids.length - 1 ? "" : " | ")
+          )}
+        </span>
         <div className="flex gap-1 items-center">
           <IoMdStar />
           <span className="text-center">
-            {props.movie.vote_average.toFixed(1)}
+            {props.movie.vote_average.toFixed(1) +
+              ` (${props.movie.vote_count})`}
           </span>
         </div>
         <span className="">
           {/* prevents movie descriptions that are too long from overflowing */}
+          {/* TODO: Reconsider this solution when I get to screen responsiveness */}
           {props.movie.overview.length <= 255
             ? props.movie.overview
             : props.movie.overview.substring(0, 255) + "..."}
